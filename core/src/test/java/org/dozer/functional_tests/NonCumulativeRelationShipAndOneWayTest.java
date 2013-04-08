a77cf657ec2b932b2d89964b7e86eb83a3af7beb
Michael Bilalis <Michael.Bilalis@lampiris.be>
2013-04-08 10:42:07 +0200

    Add bypass-already-mapped-value mapping attribute

---
+++ core/src/test/java/org/dozer/functional_tests/NonCumulativeRelationShipAndOneWayTest.java
@@ -0,0 +1,11 @@
+package org.dozer.functional_tests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: developpement
+ * Date: 28/03/13
+ * Time: 14:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class NonCumulativeRelationShipAndOneWayTest {
+}
